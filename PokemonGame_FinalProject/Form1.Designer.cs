f1199ae20fa8731c4d4bba1b267bdc185538b55a
Ayuspate412 <payush44477@gmail.com>
2024-05-29 21:30:22 -0400

    Add project files.

---
+++ PokemonGame_FinalProject/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PokemonGame_FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.characterLabel = new System.Windows.Forms.Label();
+            this.playerNameInput = new System.Windows.Forms.TextBox();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.pokemonPreview = new System.Windows.Forms.PictureBox();
+            this.playerPreview = new System.Windows.Forms.PictureBox();
+            this.squirtle = new System.Windows.Forms.Button();
+            this.bulbasaur = new System.Windows.Forms.Button();
+            this.charmander = new System.Windows.Forms.Button();
+            this.pikachu = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Location = new System.Drawing.Point(217, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(320, 131);
+            this.titleLabel.TabIndex = 0;
+            // 
+            // characterLabel
+            // 
+            this.characterLabel.BackColor = System.Drawing.Color.Transparent;
+            this.characterLabel.Font = new System.Drawing.Font("Segoe MDL2 Assets", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.characterLabel.Location = new System.Drawing.Point(8, 226);
+            this.characterLabel.Name = "characterLabel";
+            this.characterLabel.Size = new System.Drawing.Size(338, 33);
+            this.characterLabel.TabIndex = 7;
+            this.characterLabel.Text = "Enter a Name for your Character:";
+            // 
+            // playerNameInput
+            // 
+            this.playerNameInput.Location = new System.Drawing.Point(68, 376);
+            this.playerNameInput.Name = "playerNameInput";
+            this.playerNameInput.Size = new System.Drawing.Size(100, 20);
+            this.playerNameInput.TabIndex = 9;
+            // 
+            // continueButton
+            // 
+            this.continueButton.Image = global::PokemonGame_FinalProject.Properties.Resources.continueButton;
+            this.continueButton.Location = new System.Drawing.Point(59, 416);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(127, 33);
+            this.continueButton.TabIndex = 11;
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // pokemonPreview
+            // 
+            this.pokemonPreview.Location = new System.Drawing.Point(527, 226);
+            this.pokemonPreview.Name = "pokemonPreview";
+            this.pokemonPreview.Size = new System.Drawing.Size(228, 213);
+            this.pokemonPreview.TabIndex = 10;
+            this.pokemonPreview.TabStop = false;
+            // 
+            // playerPreview
+            // 
+            this.playerPreview.BackColor = System.Drawing.Color.Transparent;
+            this.playerPreview.Image = global::PokemonGame_FinalProject.Properties.Resources.down4;
+            this.playerPreview.Location = new System.Drawing.Point(85, 262);
+            this.playerPreview.Name = "playerPreview";
+            this.playerPreview.Size = new System.Drawing.Size(69, 108);
+            this.playerPreview.TabIndex = 8;
+            this.playerPreview.TabStop = false;
+            // 
+            // squirtle
+            // 
+            this.squirtle.BackColor = System.Drawing.Color.YellowGreen;
+            this.squirtle.Image = ((System.Drawing.Image)(resources.GetObject("squirtle.Image")));
+            this.squirtle.Location = new System.Drawing.Point(598, 12);
+            this.squirtle.Name = "squirtle";
+            this.squirtle.Size = new System.Drawing.Size(174, 196);
+            this.squirtle.TabIndex = 6;
+            this.squirtle.UseVisualStyleBackColor = false;
+            this.squirtle.Click += new System.EventHandler(this.squirtle_Click);
+            // 
+            // bulbasaur
+            // 
+            this.bulbasaur.BackColor = System.Drawing.Color.YellowGreen;
+            this.bulbasaur.Image = ((System.Drawing.Image)(resources.GetObject("bulbasaur.Image")));
+            this.bulbasaur.Location = new System.Drawing.Point(404, 12);
+            this.bulbasaur.Name = "bulbasaur";
+            this.bulbasaur.Size = new System.Drawing.Size(174, 196);
+            this.bulbasaur.TabIndex = 5;
+            this.bulbasaur.UseVisualStyleBackColor = false;
+            this.bulbasaur.Click += new System.EventHandler(this.bulbasaur_Click);
+            // 
+            // charmander
+            // 
+            this.charmander.BackColor = System.Drawing.Color.YellowGreen;
+            this.charmander.Image = ((System.Drawing.Image)(resources.GetObject("charmander.Image")));
+            this.charmander.Location = new System.Drawing.Point(209, 12);
+            this.charmander.Name = "charmander";
+            this.charmander.Size = new System.Drawing.Size(174, 196);
+            this.charmander.TabIndex = 4;
+            this.charmander.UseVisualStyleBackColor = false;
+            this.charmander.Click += new System.EventHandler(this.charmander_Click);
+            // 
+            // pikachu
+            // 
+            this.pikachu.BackColor = System.Drawing.Color.YellowGreen;
+            this.pikachu.Image = ((System.Drawing.Image)(resources.GetObject("pikachu.Image")));
+            this.pikachu.Location = new System.Drawing.Point(12, 12);
+            this.pikachu.Name = "pikachu";
+            this.pikachu.Size = new System.Drawing.Size(174, 196);
+            this.pikachu.TabIndex = 3;
+            this.pikachu.UseVisualStyleBackColor = false;
+            this.pikachu.Click += new System.EventHandler(this.pikachu_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.exitButton.Image = global::PokemonGame_FinalProject.Properties.Resources.Exit;
+            this.exitButton.Location = new System.Drawing.Point(305, 364);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(153, 75);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.YellowGreen;
+            this.startButton.Image = global::PokemonGame_FinalProject.Properties.Resources.Start;
+            this.startButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.startButton.Location = new System.Drawing.Point(289, 214);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(184, 114);
+            this.startButton.TabIndex = 1;
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.pokemonPreview);
+            this.Controls.Add(this.playerNameInput);
+            this.Controls.Add(this.playerPreview);
+            this.Controls.Add(this.characterLabel);
+            this.Controls.Add(this.squirtle);
+            this.Controls.Add(this.bulbasaur);
+            this.Controls.Add(this.charmander);
+            this.Controls.Add(this.pikachu);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Pokemon";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button pikachu;
+        private System.Windows.Forms.Button charmander;
+        private System.Windows.Forms.Button bulbasaur;
+        private System.Windows.Forms.Button squirtle;
+        private System.Windows.Forms.Label characterLabel;
+        private System.Windows.Forms.PictureBox playerPreview;
+        private System.Windows.Forms.TextBox playerNameInput;
+        private System.Windows.Forms.PictureBox pokemonPreview;
+        private System.Windows.Forms.Button continueButton;
+    }
+}
+
---
+++ PokemonGame_FinalProject/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonGame_FinalProject
+{
+    public partial class Form1 : Form
+    {
+        //Player
+        Rectangle player1 = new Rectangle(100, 100, 35, 40);
+
+        Image[] playerMovements = { Properties.Resources.down1, Properties.Resources.left1, Properties.Resources.right1, Properties.Resources.up1 };
+        Image playerCurrentDirection;
+
+        //Pokemon Assets
+        Image[] pokemonsList = { Properties.Resources.pikachu, Properties.Resources.squirtle, Properties.Resources.charmander, Properties.Resources.bulbasaur };
+        String actualPokemon;
+
+        //Grass
+        Rectangle grass = new Rectangle(0, 0, 1000, 600);
+        Image grassImg = Properties.Resources.bg;
+        Image forrestImg = Properties.Resources.forest;
+
+        //Brush
+        SolidBrush transparent = new SolidBrush(Color.Transparent);
+
+        //Player Speed
+        int player1Speed = 5;
+
+        int pokemonLv = 1;
+        int pokemonHealth = 1000;
+        int bossHealth = 10000;
+
+
+        //Player Movement Booleans
+        bool wPressed = false;
+        bool sPressed = false;
+        bool aPressed = false;
+        bool dPressed = false;
+        
+        //Check the start button
+        bool startButtonClicked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMainScreen();
+            playerCurrentDirection = playerMovements[1];
+            gameTimer.Enabled = false;
+
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyUp += new KeyEventHandler(Form1_KeyUp);
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            this.gameTimer.Tick += new EventHandler(gameTimer_Tick);
+
+            // Ensure the form has focus
+            this.Focus();
+        }
+
+        
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+            }
+        }
+
+        public void InitializeMainScreen()
+        {
+            pikachu.Visible = false;
+            squirtle.Visible = false;
+            charmander.Visible = false;
+            bulbasaur.Visible = false;
+            playerPreview.Visible = false;
+            pokemonPreview.Visible = false;
+            continueButton.Visible = false;
+            characterLabel.Visible = false;
+            playerNameInput.Visible = false;
+        }
+        
+        public void InitializePokemonChoosingScreen()
+        {
+            startButton.Visible = false;
+            exitButton.Visible = false;
+            titleLabel.Image = null;
+
+            pikachu.Visible = true;
+            squirtle.Visible = true;
+            charmander.Visible = true;
+            bulbasaur.Visible = true;
+            playerPreview.Visible = true;
+            pokemonPreview.Visible = true;
+            continueButton.Visible = true;
+            characterLabel.Visible = true;
+            playerNameInput.Visible = true;
+        }
+
+        public void PlayerMovement()
+        {
+            if (wPressed == true)
+            {
+                player1.Y -= player1Speed;
+                playerCurrentDirection = playerMovements[3];
+            }
+            if (sPressed ==true)
+            {
+                player1.Y += player1Speed;
+                playerCurrentDirection = playerMovements[0];
+            }
+            if (aPressed == true)
+            {
+                player1.X -= player1Speed;
+                playerCurrentDirection = playerMovements[1];
+
+            }
+            if (dPressed == true)
+            {
+                player1.X += player1Speed;
+                playerCurrentDirection = playerMovements[2];
+            }
+        }
+
+       
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(gameTimer.Enabled == false && startButtonClicked == false)
+            {
+                e.Graphics.DrawImage(forrestImg, grass);
+                titleLabel.Image = Properties.Resources.Pokemon_Sign;
+            }
+            else if(gameTimer.Enabled == false && startButtonClicked == true)
+            {
+                InitializePokemonChoosingScreen();
+                e.Graphics.DrawImage(forrestImg, grass);
+            }
+            else if (gameTimer.Enabled == true)
+            {
+                InitializeMainScreen();
+                e.Graphics.DrawImage(grassImg, grass);
+                e.Graphics.DrawImage(playerCurrentDirection, player1);
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerMovement();
+            Refresh();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButtonClicked = true;
+            startButton.Visible = false;
+            exitButton.Visible = false;
+        }
+
+
+        private void pikachu_Click(object sender, EventArgs e)
+        {
+            pokemonPreview.Image = pokemonsList[0];
+        }
+
+        private void charmander_Click(object sender, EventArgs e)
+        {
+            pokemonPreview.Image = pokemonsList[2];
+        }
+
+        private void bulbasaur_Click(object sender, EventArgs e)
+        {
+            pokemonPreview.Image = pokemonsList[3];
+        }
+
+        private void squirtle_Click(object sender, EventArgs e)
+        {
+            pokemonPreview.Image = pokemonsList[1];
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            gameTimer.Enabled = true;
+            gameTimer.Start();
+            if (pokemonPreview.Image == pokemonsList[1])
+            {
+                actualPokemon = "Pikachu";
+            }
+            else if (pokemonPreview.Image == pokemonsList[1])
+            {
+                actualPokemon = "Squirtle";
+            }
+            else if (pokemonPreview.Image == pokemonsList[2])
+            {
+                actualPokemon = "Charmander";
+            }
+            else if (pokemonPreview.Image == pokemonsList[3])
+            {
+                actualPokemon = "Bulbasaur";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S: 
+                    sPressed = true; 
+                    break;
+                case Keys.D: 
+                    dPressed = true;
+                    break;
+                case Keys.A: 
+                    aPressed = true; 
+                    break;
+            }
+        }
+    }
+}
